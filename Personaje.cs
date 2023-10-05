46933b110966723e609c23bc8c5b36596df58260
Moise-is-sleeping <mbrebre1909@g.educaand.es>
2023-10-05 16:30:20 +0200

    no entendi

---
+++ Personaje.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1;
+
+
+public interface IEquipo
+{
+    
+}
+
+enum TipoEquipo
+{
+    Casco,Peto,guantes,Bots,Amuleto,Anillo
+}
+
+
+
+
+public class Personaje
+{
+    public int Fuerza { get;  private set; }
+    public int Inteligencia { get;  private set; }
+    public int Destreza { get;  private set; }
+    public int Fe { get;  private set; }
+
+    public float Critico { get;  private set; }
+    public float DañoCritico { get;  private set; }
+    
+    private Dictionary<TipoEquipo, IEquipo> Equipo = new Dictionary<TipoEquipo, IEquipo>();
+
+    public Personaje(int fuerza, int inteligencia, int destreza, int fe, float critico, float dañoCritico)
+    {
+        Fuerza = fuerza;
+        Inteligencia = inteligencia;
+        Destreza = destreza;
+        Fe = fe;
+        Critico = critico;
+        DañoCritico = dañoCritico;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+var discoteca = new Disco("Comedia");
+Console.WriteLine(discoteca.Nombre);
+internal class Disco
+{
+    public Disco(string nombre)
+    {
+        Nombre = nombre;
+    }
+    public string Nombre { get; set; }
+
+
+}
+
+interface IFiestero
+{
+    public void TomaPirula();
+    public int VendePpirula();
+}
